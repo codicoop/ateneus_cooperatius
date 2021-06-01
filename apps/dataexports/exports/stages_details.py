afb23a7a17c80d1df95169d29f276180a7a8e0bf
Pere <pere@perepicornell.com>
2021-06-01 10:28:36 +0200

    Refactor: gestió de les dades separada en una altra class

--- apps/dataexports/exports/stages_details.py
+++ apps/dataexports/exports/stages_details.py
@@ -8,14 +8,13 @@ from dataexports.exports.manager import ExcelExportManager
 
 
 class ExportStagesDetails:
-    organizer_ateneu_id = 5
-    organizer_circle_migrations_id = 2
-    organizer_circle_eco_id = 6
     circles_data = None
 
     def __init__(self, export_obj):
         self.export_manager = ExcelExportManager(export_obj)
-        self.circles_data = self.get_circles_data()
+        self.circles_data = CirclesDataManager(
+            self.export_manager.subsidy_period
+        ).get_circles_data()
 
     def export(self):
         """ Each function here called handles the creation of one of the
@@ -26,7 +25,7 @@ class ExportStagesDetails:
         return self.export_manager.return_document("detalls_acompanyaments")
 
     def export_circles(self):
-        self.export_manager.worksheet.title = "Acompanyaments covid"
+        self.export_manager.worksheet.title = "Ateneu-Cercles"
         self.export_manager.row_number = 1
 
         columns = [
@@ -36,130 +35,10 @@ class ExportStagesDetails:
             ("Sense certificat BO", 20),
         ]
         self.export_manager.create_columns(columns)
-
-        self.circles_rows()
-
-    def get_axis_key(self, axis, qs):
-        for key, value in qs:
-            if value['axis'] == axis:
-                return key
-        return None
-
-    @staticmethod
-    def none_as_zero(value):
-        return value if value else 0
-
-    def format_circles_data(self, data):
-        template = self.get_data_structure()
-        for item in data:
-            print("Item:")
-            pprint(item["hours_ateneu_certified"])
-            # Ateneu
-            template["ateneu"][item["axis"]][2] = self.none_as_zero(
-                item["hours_ateneu_certified"]
-            )
-            template["ateneu"][item["axis"]][3] = self.none_as_zero(
-                item["hours_ateneu_uncertified"]
-            )
-            if isinstance(item["hours_ateneu_certified"], numbers.Number):
-                template["ateneu"]["total"][2] += item["hours_ateneu_certified"]
-            if isinstance(item["hours_ateneu_uncertified"], numbers.Number):
-                template["ateneu"]["total"][3] += item["hours_ateneu_uncertified"]
-
-            # Cercle Migracions
-            template["circle_migrations"][item["axis"]][2] = self.none_as_zero(
-                item["hours_circle_migrations_certified"]
-            )
-            template["circle_migrations"][item["axis"]][3] = self.none_as_zero(
-                item["hours_circle_migrations_uncertified"]
-            )
-            if isinstance(item["hours_circle_migrations_certified"], numbers.Number):
-                template["circle_migrations"]["total"][2] += \
-                    item["hours_circle_migrations_certified"]
-            if isinstance(item["hours_circle_migrations_uncertified"], numbers.Number):
-                template["circle_migrations"]["total"][3] += \
-                    item["hours_circle_migrations_uncertified"]
-
-            # Cercle canvi ecosocial
-            template["circle_eco"][item["axis"]][2] = self.none_as_zero(
-                item["hours_circle_eco_certified"]
-            )
-            template["circle_eco"][item["axis"]][3] = self.none_as_zero(
-                item["hours_circle_eco_uncertified"]
-            )
-            if isinstance(item["hours_circle_eco_certified"], numbers.Number):
-                template["circle_eco"]["total"][2] += \
-                    item["hours_circle_eco_certified"]
-            if isinstance(item["hours_circle_eco_uncertified"], numbers.Number):
-                template["circle_eco"]["total"][3] += \
-                    item["hours_circle_eco_uncertified"]
-        return template
-
-    def get_circles_data(self):
-        query = {
-            'hours_ateneu_certified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(stage_organizer=self.organizer_ateneu_id) &
-                    Q(scanned_certificate__isnull=False) &
-                    ~Q(scanned_certificate__exact='')
-                )
-            ),
-            'hours_ateneu_uncertified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(scanned_certificate__isnull=True) |
-                    Q(scanned_certificate__exact='')
-                )
-            ),
-            'hours_circle_migrations_certified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(stage_organizer=self.organizer_circle_migrations_id) &
-                    Q(scanned_certificate__isnull=False) &
-                    ~Q(scanned_certificate__exact='')
-                )
-            ),
-            'hours_circle_migrations_uncertified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(scanned_certificate__isnull=True) |
-                    Q(scanned_certificate__exact='')
-                )
-            ),
-            'hours_circle_eco_certified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(stage_organizer=self.organizer_circle_eco_id) &
-                    Q(scanned_certificate__isnull=False) &
-                    ~Q(scanned_certificate__exact='')
-                )
-            ),
-            'hours_circle_eco_uncertified': Sum(
-                'stage_sessions__hours',
-                filter=(
-                    Q(scanned_certificate__isnull=True) |
-                    Q(scanned_certificate__exact='')
-                )
-            ),
-        }
-        qs = ProjectStage.objects.filter(
-            subsidy_period=self.export_manager.subsidy_period,
-        )
-        qs = (
-            qs
-                .values('axis')
-                .annotate(**query)
-        )
-        # Disabling order_by because it breaks the group_by.
-        qs = qs.order_by()
-        data = self.format_circles_data(qs)
-        return data
-
-    def circles_rows(self):
         self.circles_ateneu_rows()
         self.circles_circle1_rows()
         self.circles_circle2_rows()
+        # self.circles_ateneu_users_rows()
 
     def circles_ateneu_rows(self):
         rows = list(self.circles_data["ateneu"].values())
@@ -199,6 +78,17 @@ class ExportStagesDetails:
         for row in rows:
             self.export_manager.row_number += 1
             self.export_manager.fill_row_data(row)
+
+
+class CirclesDataManager:
+    organizer_ateneu_id = 5
+    organizer_circle_migrations_id = 2
+    organizer_circle_eco_id = 6
+    subsidy_period = None
+
+    def __init__(self, subsidy_period):
+        print(subsidy_period)
+        self.subsidy_period = subsidy_period
 
     @staticmethod
     def get_data_structure():
@@ -318,3 +208,114 @@ class ExportStagesDetails:
             ],
           },
         }
+
+    def get_circles_data(self):
+        query = {
+            'hours_ateneu_certified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(stage_organizer=self.organizer_ateneu_id) &
+                    Q(scanned_certificate__isnull=False) &
+                    ~Q(scanned_certificate__exact='')
+                )
+            ),
+            'hours_ateneu_uncertified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(scanned_certificate__isnull=True) |
+                    Q(scanned_certificate__exact='')
+                )
+            ),
+            'hours_circle_migrations_certified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(stage_organizer=self.organizer_circle_migrations_id) &
+                    Q(scanned_certificate__isnull=False) &
+                    ~Q(scanned_certificate__exact='')
+                )
+            ),
+            'hours_circle_migrations_uncertified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(scanned_certificate__isnull=True) |
+                    Q(scanned_certificate__exact='')
+                )
+            ),
+            'hours_circle_eco_certified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(stage_organizer=self.organizer_circle_eco_id) &
+                    Q(scanned_certificate__isnull=False) &
+                    ~Q(scanned_certificate__exact='')
+                )
+            ),
+            'hours_circle_eco_uncertified': Sum(
+                'stage_sessions__hours',
+                filter=(
+                    Q(scanned_certificate__isnull=True) |
+                    Q(scanned_certificate__exact='')
+                )
+            ),
+        }
+        qs = ProjectStage.objects.filter(
+            subsidy_period=self.subsidy_period,
+        )
+        qs = (
+            qs
+                .values('axis')
+                .annotate(**query)
+        )
+        # Disabling order_by because it breaks the group_by.
+        qs = qs.order_by()
+        data = self.format_circles_data(qs)
+        return data
+
+    def format_circles_data(self, data):
+        template = self.get_data_structure()
+        for item in data:
+            print("Item:")
+            pprint(item["hours_ateneu_certified"])
+            # Ateneu
+            template["ateneu"][item["axis"]][2] = self.none_as_zero(
+                item["hours_ateneu_certified"]
+            )
+            template["ateneu"][item["axis"]][3] = self.none_as_zero(
+                item["hours_ateneu_uncertified"]
+            )
+            if isinstance(item["hours_ateneu_certified"], numbers.Number):
+                template["ateneu"]["total"][2] += item["hours_ateneu_certified"]
+            if isinstance(item["hours_ateneu_uncertified"], numbers.Number):
+                template["ateneu"]["total"][3] += item["hours_ateneu_uncertified"]
+
+            # Cercle Migracions
+            template["circle_migrations"][item["axis"]][2] = self.none_as_zero(
+                item["hours_circle_migrations_certified"]
+            )
+            template["circle_migrations"][item["axis"]][3] = self.none_as_zero(
+                item["hours_circle_migrations_uncertified"]
+            )
+            if isinstance(item["hours_circle_migrations_certified"], numbers.Number):
+                template["circle_migrations"]["total"][2] += \
+                    item["hours_circle_migrations_certified"]
+            if isinstance(item["hours_circle_migrations_uncertified"], numbers.Number):
+                template["circle_migrations"]["total"][3] += \
+                    item["hours_circle_migrations_uncertified"]
+
+            # Cercle canvi ecosocial
+            template["circle_eco"][item["axis"]][2] = self.none_as_zero(
+                item["hours_circle_eco_certified"]
+            )
+            template["circle_eco"][item["axis"]][3] = self.none_as_zero(
+                item["hours_circle_eco_uncertified"]
+            )
+            if isinstance(item["hours_circle_eco_certified"], numbers.Number):
+                template["circle_eco"]["total"][2] += \
+                    item["hours_circle_eco_certified"]
+            if isinstance(item["hours_circle_eco_uncertified"], numbers.Number):
+                template["circle_eco"]["total"][3] += \
+                    item["hours_circle_eco_uncertified"]
+        return template
+
+    @staticmethod
+    def none_as_zero(value):
+        return value if value else 0
