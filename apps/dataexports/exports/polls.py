dc3b120d2d872f82d8609e26a9245b40b96dbc60
Pere <pere@perepicornell.com>
2021-10-28 16:41:12 +0200

    Primer full amb l'esquelet de rows muntat.

--- apps/dataexports/exports/polls.py
+++ apps/dataexports/exports/polls.py
@@ -2,15 +2,20 @@ from coopolis.models import ActivityPoll
 from dataexports.exports.manager import ExcelExportManager
 
 
-class GlobalReportRow:
+class BaseRow:
+    def get_columns(self) -> list:
+        return []
+
+
+class GlobalReportRow(BaseRow):
     def __init__(
             self,
-            title,
-            ateneu=0,
-            cercle1=0,
-            cercle2=0,
-            cercle3=0,
-            cercle4=0
+            title: str,
+            ateneu: int = 0,
+            cercle1: int = 0,
+            cercle2: int = 0,
+            cercle3: int = 0,
+            cercle4: int = 0,
     ):
         self.title = title
         self.ateneu = ateneu
@@ -18,6 +23,28 @@ class GlobalReportRow:
         self.cercle2 = cercle2
         self.cercle3 = cercle3
         self.cercle4 = cercle4
+
+    def get_columns(self) -> list:
+        return [
+            self.title,
+            self.ateneu,
+            self.cercle1,
+            self.cercle2,
+            self.cercle3,
+            self.cercle4,
+        ]
+
+
+class EmptyRow(BaseRow):
+    pass
+
+
+class TitleRow(BaseRow):
+    def __init__(self, title: str):
+        self.title = title
+
+    def get_columns(self) -> list:
+        return [self.title, ]
 
 
 class ExportPolls:
@@ -55,44 +82,41 @@ class ExportPolls:
     def polls_rows(self):
         rows = [
             GlobalReportRow("Nombre d'enquestes de satisfacció valorades"),
+            EmptyRow(),
             GlobalReportRow("Valoracions globals"),
             GlobalReportRow("Valoració global de les actuacions"),
-            GlobalReportRow("Organització"),
+            EmptyRow(),
+            TitleRow("Organització"),
             GlobalReportRow("La durada ha estat l'adequada?"),
             GlobalReportRow("Els horaris han estat adequats?"),
             GlobalReportRow("Informació necessària per fer l'activitat"),
             GlobalReportRow("S'han complert les dates, horaris, etc..."),
             GlobalReportRow("Els espais han estat adequats (sales, aules, plataforma digital...)"),
-            GlobalReportRow("Continguts"),
+            EmptyRow(),
+            TitleRow("Continguts"),
             GlobalReportRow("Els continguts han estat adequats"),
             GlobalReportRow("Metodologia"),
             GlobalReportRow("La metodologia ha estat coherent amb els objectius"),
             GlobalReportRow("La metodologia ha permès obtenir millors resultats"),
             GlobalReportRow("El sistema de participació i resolució de dubtes ha estat adequat?"),
-            GlobalReportRow("Valoració de la persona formadora"),
+            EmptyRow(),
+            TitleRow("Valoració de la persona formadora"),
             GlobalReportRow("Ha mostrat coneixements i experiència sobre el tema?"),
             GlobalReportRow("Ha aconseguit resoldre els problemes i dubtes que s’ha plantejat?"),
             GlobalReportRow("El professional ha mostrat competències comunicatives?"),
-            GlobalReportRow("Utilitat del curs"),
+            EmptyRow(),
+            TitleRow("Utilitat del curs"),
             GlobalReportRow("Ha satisfet les meves expectatives"),
             GlobalReportRow("He incorporat eines per aplicar a nous projectes"),
             GlobalReportRow("M'ha permès conèixer persones afins"),
             GlobalReportRow("Abans del curs, teníeu ganes/necessitats d'engegar algun projecte cooperatiu"),
             GlobalReportRow("I després?"),
-            GlobalReportRow("Valoració global"),
+            EmptyRow(),
+            TitleRow("Valoració global"),
             GlobalReportRow("Grau de satisfacció general"),
         ]
         # polls_obj = self.global_report_obj()
 
         for row in rows:
             self.export_manager.row_number += 1
-            self.export_manager.fill_row_data(
-                [
-                    row.title,
-                    row.ateneu,
-                    row.cercle1,
-                    row.cercle2,
-                    row.cercle3,
-                    row.cercle4,
-                ]
-            )
+            self.export_manager.fill_row_data(row.get_columns())
