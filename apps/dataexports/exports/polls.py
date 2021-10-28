3283987dc00e5298df6302899196fbed3844e2a9
Pere <pere@perepicornell.com>
2021-10-28 16:25:21 +0200

    Primer full amb l'esquelet de rows muntat.

--- apps/dataexports/exports/polls.py
+++ apps/dataexports/exports/polls.py
@@ -1,4 +1,23 @@
+from coopolis.models import ActivityPoll
 from dataexports.exports.manager import ExcelExportManager
+
+
+class GlobalReportRow:
+    def __init__(
+            self,
+            title,
+            ateneu=0,
+            cercle1=0,
+            cercle2=0,
+            cercle3=0,
+            cercle4=0
+    ):
+        self.title = title
+        self.ateneu = ateneu
+        self.cercle1 = cercle1
+        self.cercle2 = cercle2
+        self.cercle3 = cercle3
+        self.cercle4 = cercle4
 
 
 class ExportPolls:
@@ -17,40 +36,63 @@ class ExportPolls:
         self.export_manager.row_number = 1
 
         columns = [
-            ("Projecte", 20),
-            ("Data d'inici", 20),
-            ("Tipus d'acompanyament", 20),
-            ("Responsable", 20),
-            ("Número d'hores", 20),
-            ("Eix-subeix", 20),
-            ("Convocatòria", 20),
-            ("Participants", 20),
-            ("Ateneu/Cercle", 20),
+            ("", 60),
+            ("Ateneu Cooperatiu", 20),
+            ("Cercle 1", 20),
+            ("Cercle 2", 20),
+            ("Cercle 3", 20),
+            ("Cercle 4", 20),
         ]
         self.export_manager.create_columns(columns)
 
         self.polls_rows()
 
-    def get_stages_obj(self):
-        return ProjectStage.objects.order_by('date_start').filter(
-            subsidy_period=self.export_manager.subsidy_period,
-            covid_crisis=True
+    def global_report_obj(self):
+        return ActivityPoll.objects.filter(
+            created__range=self.export_manager.subsidy_period_range,
         )
 
     def polls_rows(self):
-        stages_obj = self.get_stages_obj()
+        rows = [
+            GlobalReportRow("Nombre d'enquestes de satisfacció valorades"),
+            GlobalReportRow("Valoracions globals"),
+            GlobalReportRow("Valoració global de les actuacions"),
+            GlobalReportRow("Organització"),
+            GlobalReportRow("La durada ha estat l'adequada?"),
+            GlobalReportRow("Els horaris han estat adequats?"),
+            GlobalReportRow("Informació necessària per fer l'activitat"),
+            GlobalReportRow("S'han complert les dates, horaris, etc..."),
+            GlobalReportRow("Els espais han estat adequats (sales, aules, plataforma digital...)"),
+            GlobalReportRow("Continguts"),
+            GlobalReportRow("Els continguts han estat adequats"),
+            GlobalReportRow("Metodologia"),
+            GlobalReportRow("La metodologia ha estat coherent amb els objectius"),
+            GlobalReportRow("La metodologia ha permès obtenir millors resultats"),
+            GlobalReportRow("El sistema de participació i resolució de dubtes ha estat adequat?"),
+            GlobalReportRow("Valoració de la persona formadora"),
+            GlobalReportRow("Ha mostrat coneixements i experiència sobre el tema?"),
+            GlobalReportRow("Ha aconseguit resoldre els problemes i dubtes que s’ha plantejat?"),
+            GlobalReportRow("El professional ha mostrat competències comunicatives?"),
+            GlobalReportRow("Utilitat del curs"),
+            GlobalReportRow("Ha satisfet les meves expectatives"),
+            GlobalReportRow("He incorporat eines per aplicar a nous projectes"),
+            GlobalReportRow("M'ha permès conèixer persones afins"),
+            GlobalReportRow("Abans del curs, teníeu ganes/necessitats d'engegar algun projecte cooperatiu"),
+            GlobalReportRow("I després?"),
+            GlobalReportRow("Valoració global"),
+            GlobalReportRow("Grau de satisfacció general"),
+        ]
+        # polls_obj = self.global_report_obj()
 
-        for stage in stages_obj:
+        for row in rows:
             self.export_manager.row_number += 1
-            row = [
-                stage.project.name,
-                stage.date_start,
-                stage.get_stage_type_display(),
-                str(stage.stage_responsible),
-                stage.hours_sum(),
-                stage.axis_summary(),
-                str(stage.subsidy_period),
-                len(stage.involved_partners.all()),
-                str(stage.stage_organizer),
-            ]
-            self.export_manager.fill_row_data(row)
+            self.export_manager.fill_row_data(
+                [
+                    row.title,
+                    row.ateneu,
+                    row.cercle1,
+                    row.cercle2,
+                    row.cercle3,
+                    row.cercle4,
+                ]
+            )
