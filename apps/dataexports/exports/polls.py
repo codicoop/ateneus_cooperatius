5ee7588d5b2689794c9575e5d31ca8dac4652f8c
Pere <pere@perepicornell.com>
2021-10-29 12:56:05 +0200

    Enquestes amb l'estructura per calcular les mitjanes funcionant i la 1a fila d'exemple.

--- apps/dataexports/exports/polls.py
+++ apps/dataexports/exports/polls.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from django.db.models import Count
+from django.db.models import Count, Avg, F, IntegerField
 
 from cc_courses.models import Organizer
 from coopolis.models import ActivityPoll
@@ -21,6 +21,8 @@ class GlobalReportRow(BaseRow):
             cercle2: int = 0,
             cercle3: int = 0,
             cercle4: int = 0,
+            values_dict: dict = None,
+            values_dict_field: str = None,
     ):
         self.title = title
         self.ateneu = ateneu
@@ -28,15 +30,22 @@ class GlobalReportRow(BaseRow):
         self.cercle2 = cercle2
         self.cercle3 = cercle3
         self.cercle4 = cercle4
+        if values_dict and values_dict_field:
+            print(values_dict["cercle1"])
+            self.ateneu = values_dict["ateneu"].get(values_dict_field)
+            self.cercle1 = values_dict["cercle1"].get(values_dict_field)
+            self.cercle2 = values_dict["cercle2"].get(values_dict_field)
+            self.cercle3 = values_dict["cercle3"].get(values_dict_field)
+            self.cercle4 = values_dict["cercle4"].get(values_dict_field)
 
     def get_columns(self) -> list:
         return [
             self.title,
-            self.ateneu,
-            self.cercle1,
-            self.cercle2,
-            self.cercle3,
-            self.cercle4,
+            round(self.ateneu, 1),
+            round(self.cercle1, 1),
+            round(self.cercle2, 1),
+            round(self.cercle3, 1),
+            round(self.cercle4, 1),
         ]
 
 
@@ -84,6 +93,7 @@ class ExportPolls:
     def global_report_obj(self):
         querysets = []
         for organizer in self.organizers.values():
+            pprint(self.export_manager.subsidy_period_range)
             querysets.append(
                 ActivityPoll.objects.filter(
                     created__range=self.export_manager.subsidy_period_range,
@@ -94,49 +104,126 @@ class ExportPolls:
         # qs.order_by()
         return querysets
 
+    def get_averages_qs(self, queryset):
+        return queryset.aggregate(
+            Avg("duration"),
+            Avg("hours"),
+            Avg("information"),
+            Avg("on_schedule"),
+            Avg("included_resources"),
+            Avg("space_adequation"),
+            Avg("contents"),
+            Avg("methodology_fulfilled_objectives"),
+            Avg("methodology_better_results"),
+            Avg("participation_system"),
+        )
+
     def polls_rows(self):
         querysets = self.global_report_obj()
-        pprint(querysets)
+        total_organizers = len(querysets)
+        if not total_organizers:
+            return
+        averages = {
+            "ateneu": self.get_averages_qs(querysets[0]),
+            "cercle1": {},
+            "cercle2": {},
+            "cercle3": {},
+            "cercle4": {},
+        }
+        if total_organizers > 1:
+            averages["cercle1"] = self.get_averages_qs(querysets[1])
+        if total_organizers > 2:
+            averages["cercle2"] = self.get_averages_qs(querysets[2])
+        if total_organizers > 3:
+            averages["cercle3"] = self.get_averages_qs(querysets[3])
+        if total_organizers > 4:
+            averages["cercle4"] = self.get_averages_qs(querysets[4])
+        print("cercle4 després d'assignar-lo:", averages["cercle4"])
+
+        # pprint(averages)
         rows = [
             GlobalReportRow(
                 "Nombre d'enquestes de satisfacció valorades",
                 querysets[0].count(),
-                querysets[1].count() if len(querysets) > 0 else "-",
-                querysets[2].count() if len(querysets) > 1 else "-",
-                querysets[3].count() if len(querysets) > 2 else "-",
+                querysets[1].count() if total_organizers > 1 else "-",
+                querysets[2].count() if total_organizers > 2 else "-",
+                querysets[3].count() if total_organizers > 3 else "-",
+                querysets[4].count() if total_organizers > 4 else "-",
             ),
             EmptyRow(),
-            GlobalReportRow("Valoracions globals"),
+            GlobalReportRow(
+                "Valoracions globals",
+            ),
             GlobalReportRow("Valoració global de les actuacions"),
             EmptyRow(),
             TitleRow("Organització"),
-            GlobalReportRow("La durada ha estat l'adequada?"),
-            GlobalReportRow("Els horaris han estat adequats?"),
-            GlobalReportRow("Informació necessària per fer l'activitat"),
-            GlobalReportRow("S'han complert les dates, horaris, etc..."),
-            GlobalReportRow("Els espais han estat adequats (sales, aules, plataforma digital...)"),
+            GlobalReportRow(
+                "La durada ha estat l'adequada?",
+                values_dict=averages,
+                values_dict_field="duration__avg",
+            ),
+            GlobalReportRow(
+                "Els horaris han estat adequats?",
+            ),
+            GlobalReportRow(
+                "Informació necessària per fer l'activitat",
+            ),
+            GlobalReportRow(
+                "S'han complert les dates, horaris, etc...",
+            ),
+            GlobalReportRow(
+                "Els espais han estat adequats (sales, aules, plataforma digital...)",
+            ),
             EmptyRow(),
             TitleRow("Continguts"),
-            GlobalReportRow("Els continguts han estat adequats"),
-            GlobalReportRow("Metodologia"),
-            GlobalReportRow("La metodologia ha estat coherent amb els objectius"),
-            GlobalReportRow("La metodologia ha permès obtenir millors resultats"),
-            GlobalReportRow("El sistema de participació i resolució de dubtes ha estat adequat?"),
+            GlobalReportRow(
+                "Els continguts han estat adequats",
+            ),
+            GlobalReportRow(
+                "Metodologia",
+            ),
+            GlobalReportRow(
+                "La metodologia ha estat coherent amb els objectius",
+            ),
+            GlobalReportRow(
+                "La metodologia ha permès obtenir millors resultats",
+            ),
+            GlobalReportRow(
+                "El sistema de participació i resolució de dubtes ha estat adequat?",
+            ),
             EmptyRow(),
             TitleRow("Valoració de la persona formadora"),
-            GlobalReportRow("Ha mostrat coneixements i experiència sobre el tema?"),
-            GlobalReportRow("Ha aconseguit resoldre els problemes i dubtes que s’ha plantejat?"),
-            GlobalReportRow("El professional ha mostrat competències comunicatives?"),
+            GlobalReportRow(
+                "Ha mostrat coneixements i experiència sobre el tema?",
+            ),
+            GlobalReportRow(
+                "Ha aconseguit resoldre els problemes i dubtes que s’ha plantejat?",
+            ),
+            GlobalReportRow(
+                "El professional ha mostrat competències comunicatives?",
+            ),
             EmptyRow(),
             TitleRow("Utilitat del curs"),
-            GlobalReportRow("Ha satisfet les meves expectatives"),
-            GlobalReportRow("He incorporat eines per aplicar a nous projectes"),
-            GlobalReportRow("M'ha permès conèixer persones afins"),
-            GlobalReportRow("Abans del curs, teníeu ganes/necessitats d'engegar algun projecte cooperatiu"),
-            GlobalReportRow("I després?"),
+            GlobalReportRow(
+                "Ha satisfet les meves expectatives",
+            ),
+            GlobalReportRow(
+                "He incorporat eines per aplicar a nous projectes",
+            ),
+            GlobalReportRow(
+                "M'ha permès conèixer persones afins",
+            ),
+            GlobalReportRow(
+                "Abans del curs, teníeu ganes/necessitats d'engegar algun projecte cooperatiu",
+            ),
+            GlobalReportRow(
+                "I després?",
+            ),
             EmptyRow(),
             TitleRow("Valoració global"),
-            GlobalReportRow("Grau de satisfacció general"),
+            GlobalReportRow(
+                "Grau de satisfacció general",
+            ),
         ]
 
         for row in rows:
