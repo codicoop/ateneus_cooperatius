e7c719e63958d0631c337bbc96337121b9edbfd5
Pere <pere@perepicornell.com>
2022-03-16 16:50:42 +0100

    Added default message to exception

--- src/apps/dataexports/exports/exceptions.py
+++ src/apps/dataexports/exports/exceptions.py
@@ -1,6 +1,9 @@
 class MissingOrganizers(Exception):
     """The polls report is based in Organizers and no Organizer exists."""
 
+    def __init__(self, message="'organizer' data missing."):
+        super().__init__(message)
+
 
 class AxisDoesNotExistException(Exception):
     """Trying to resolve the title of an axis that does not exist in settings."""
