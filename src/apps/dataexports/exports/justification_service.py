f78ea8905baaa3126ee2b75e4d04c2c64843133a
Pere Picornell <pere@perepicornell.com>
2022-11-04 17:14:58 +0100

    Actuacions: camps buits en vermell

--- src/apps/dataexports/exports/justification_service.py
+++ src/apps/dataexports/exports/justification_service.py
@@ -266,15 +266,23 @@ class ExportJustificationService:
                     'row_number'
                 ] = self.export_manager.row_number - 1
 
-                service = item.get_service_display() if item.service else ""
-                sub_service = item.get_sub_service_display() if item.sub_service else ""
+                service = (
+                    item.get_service_display()
+                    if item.service
+                    else ("", True)
+                )
+                sub_service = (
+                    item.get_sub_service_display()
+                    if item.sub_service
+                    else ("", True)
+                )
                 town = ("", True)
                 if item.project.town:
                     town = str(item.project.town)
                 circle = (
                      CirclesChoices(item.circle).label
                      if item.circle is not None
-                     else ""
+                     else ("", True)
                 )
 
                 row = [
@@ -304,8 +312,16 @@ class ExportJustificationService:
         for item in obj:
             self.export_manager.row_number += 1
 
-            service = item.get_service_display() if item.service else ""
-            sub_service = item.get_sub_service_display() if item.sub_service else ""
+            service = (
+                item.get_service_display()
+                if item.service
+                else ("", True)
+            )
+            sub_service = (
+                item.get_sub_service_display()
+                if item.sub_service
+                else ("", True)
+            )
             town = ("", True)
             if item.place and item.place.town:
                 town = str(item.place.town)
@@ -318,7 +334,7 @@ class ExportJustificationService:
             circle = (
                  CirclesChoices(item.circle).label
                  if item.circle is not None
-                 else ""
+                 else ("", True)
             )
 
             row = [
@@ -376,15 +392,23 @@ class ExportJustificationService:
             stage = stages.all()[0]
 
             self.export_manager.row_number += 1
-            service = stage.get_service_display() if stage.service else ""
-            sub_service = stage.get_sub_service_display() if stage.sub_service else ""
+            service = (
+                stage.get_service_display()
+                if stage.service
+                else ("", True)
+            )
+            sub_service = (
+                stage.get_sub_service_display()
+                if stage.sub_service
+                else ("", True)
+            )
             town = ("", True)
             if project.town:
                 town = str(project.town)
             circle = (
                  CirclesChoices(stage.circle).label
                  if stage.circle is not None
-                 else ""
+                 else ("", True)
             )
 
             row = [
