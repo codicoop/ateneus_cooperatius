28c421fba7a963cc04b5caf25b02246ddb4300d8
Pere Picornell <pere@perepicornell.com>
2022-11-07 16:22:45 +0100

    Afegits permissos 'view_activityfile', 'delete_activityfile', 'change_activityfile', 'add_activityfile',

--- src/apps/coopolis/management/commands/normalizedata.py
+++ src/apps/coopolis/management/commands/normalizedata.py
@@ -191,7 +191,7 @@ class Command(BaseCommand):
         print('Permisos del grup Permisos base actualitzats.')
 
         # formació / sessions
-        group, created = Group.objects.get_or_create(name="Gestió d'accions i sessions")
+        group, created = Group.objects.get_or_create(name="")
         add_thing = Permission.objects.filter(
             codename__in=[
                 'add_activity', 'change_activity', 'delete_activity', 'view_activity', 'add_course',
@@ -204,6 +204,7 @@ class Command(BaseCommand):
                 'change_thumbnaildimensions', 'delete_thumbnaildimensions', 'view_thumbnaildimensions',
                 'view_activityenrolled', 'delete_activityenrolled', 'change_activityenrolled', 'add_activityenrolled',
                 'view_activityresourcefile', 'delete_activityresourcefile', 'change_activityresourcefile', 'add_activityresourcefile',
+                'view_activityfile', 'delete_activityfile', 'change_activityfile', 'add_activityfile',
                 'view_activitypoll',
             ]
         )
