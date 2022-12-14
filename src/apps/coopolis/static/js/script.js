62e6665f8c59a6549de0d5ed96cc151b0560c8bf
MartaRF3 <ricartfm@gmail.com>
2022-12-14 11:40:02 +0100

    #21 Afegir login flotant

--- src/apps/coopolis/static/js/script.js
+++ src/apps/coopolis/static/js/script.js
@@ -7,3 +7,12 @@ function closeMenu() {
   const menu = document.querySelector(".sidebar")
   menu.classList.remove("menu-is-open")
 }
+// Mostrar el formulari de login
+function showLoginFloat() {
+  const login = document.querySelector(".login")
+  login.classList.remove("is-hidden")
+}
+function hideLoginFloat() {
+  const login = document.querySelector(".login")
+  login.classList.add("is-hidden")
+}
