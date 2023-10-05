f3ba04ae18ac315cd34ac6e75f9edd2a6664c8e9
Pere Picornell <pere@perepicornell.com>
2023-10-05 16:35:04 +0200

    Afegida migració de dades via comandament, per vincular les poblacions amb les comarques i actualitzar-ne el nom.

--- src/apps/coopolis/management/commands/importcounties.py
+++ src/apps/coopolis/management/commands/importcounties.py
@@ -3,15 +3,18 @@ from urllib.request import urlopen
 
 from django.core.management.base import BaseCommand
 
-from apps.coopolis.models.general import County
+from apps.coopolis.models.general import County, Town
 
 
 class Command(BaseCommand):
     help = "Imports Counties"
     counties_source = "https://analisi.transparenciacatalunya.cat/resource/gsjn-sema.json"
+    towns_source = "https://analisi.transparenciacatalunya.cat/resource/9aju-tpwc.json"
 
     def handle(self, *args, **options):
-        self.import_counties()
+        # Not using this one because we did it in a data migration
+        # self.import_counties()
+        self.import_towns()
 
     def import_counties(self):
         counties = json.loads(urlopen(self.counties_source).read())
@@ -22,3 +25,51 @@ class Command(BaseCommand):
                 name=county["nom"],
             )
             print(f"Creada comarca: {obj.name}")
+
+    def import_towns(self):
+        towns = json.loads(urlopen(self.towns_source).read())
+        total_matches = 0
+        total_not_matched = 0
+        for town in towns:
+            name = town["nom"]
+            if self.get_hardcoded_town(town["nom"]):
+                name = self.get_hardcoded_town(town["nom"])
+                print(f"Name of {town['nom']} resolved to {name}")
+            if name == "Esquirol, l'":
+                if not Town.objects.filter(name=name).exists():
+                    # Needs to be added to databaseº
+                    Town.objects.create(
+                        name=name,
+                        name_for_justification=name,
+                        county_id=int(town["codi_comarca"]),
+                    )
+                continue
+
+            res = Town.objects.filter(name__unaccent__iexact=name)
+            if res:
+                print(f"Matched! {len(res)} coincidències.")
+                for match in res:
+                    total_matches += 1
+                    print(f"Matched! {town['nom']} == {match.name}")
+                    match.name_for_justification = match.name
+                    match.name = town["nom"]
+                    match.county_id = int(town["codi_comarca"])
+                    match.save()
+            else:
+                total_not_matched += 1
+                print(f"Not matched: {town['nom']}")
+
+        print(f"Total poblacions: {Town.objects.all().count()}")
+        print(f"Total de coincidències: {total_matches}")
+        print(f"Total de NO coincidències: {total_not_matched}")
+
+    def get_hardcoded_town(self, name):
+        equivalencies = {
+            "Brunyola i Sant Martí Sapresa": "BRUNYOLA",
+            "Calonge i Sant Antoni": "CALONGE",
+            "Bigues i Riells del Fai": "BIGUES I RIELLS",
+            "Ràpita, la": "SANT CARLES DE LA RAPITA",
+            "Roda de Berà": "RODA DE BARA",
+            "Saus, Camallera i Llampaies": "SAUS,CAMALLERA I LLAMPAIES",
+        }
+        return equivalencies.get(name)
