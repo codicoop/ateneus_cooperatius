82e1ddb8452d59ea25255909a722cab55baf420b
Pere Picornell <pere@perepicornell.com>
2023-10-05 12:59:12 +0200

    Creat model County

--- src/apps/coopolis/models/general.py
+++ src/apps/coopolis/models/general.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class County(models.Model):
+    name = models.CharField("nom", max_length=50)
+
+    class Meta:
+        verbose_name = "comarca"
+        verbose_name_plural = "comarques"
+        ordering = ["name", ]
+
+    def __str__(self):
+        return self.name
+
+
 class Town(models.Model):
     class Meta:
         verbose_name = "població"
