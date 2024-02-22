3d8e09b71e35765e1f49b168fc601f0c31c0efe5
eliashz <eliashz@gmail.com>
2024-02-22 13:12:09 +0100

    Quitar views de ProjectStage de ProjectFormView

--- src/apps/coopolis/views/ProjectFormView.py
+++ src/apps/coopolis/views/ProjectFormView.py
@@ -1,19 +1,12 @@
 from constance import config
 from django import urls
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 
-from apps.coopolis.forms import (
-    ProjectCharacteristicsForm,
-    ProjectForm,
-    ProjectStageAttachForm,
-    ProjectStageInitialPetitionForm,
-    ProjectStageStartForm,
-)
+from apps.coopolis.forms import ProjectForm
+
 from apps.coopolis.models import Project
 from apps.coopolis.views import LoginSignupContainerView
 from conf.custom_mail_manager import MyMailTemplate
@@ -75,74 +68,3 @@ class ProjectInfoView(LoginSignupContainerView):
     #             return HttpResponseRedirect(urls.reverse("new_project"))
     #     return super().get(self, request, *args, **kwargs)
 
-
-def project_stage_view(request):
-    projects = Project.objects.filter(partners=request.user)
-    return render(request, "project_.html", {"projects": projects})
-
-
-def project_stage_start_view(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    return render(request, "project_stage_start.html", {"project": project})
-
-
-def project_stage_data_view(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    form = ProjectStageStartForm(request.POST, instance=project)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("project_stage_attatch", pk=pk)
-    else:
-        form = ProjectStageStartForm(instance=project)
-    return render(
-        request, "project_stage_data.html", {"project": project, "form": form}
-    )
-
-
-def project_stage_attatch_view(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    form = ProjectStageAttachForm(request.POST, instance=project)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("project_stage_initial_petition", pk=pk)
-    else:
-        form = ProjectStageAttachForm(instance=project)
-    return render(
-        request, "project_stage_attatch.html", {"project": project, "form": form}
-    )
-
-
-def project_stage_initial_petition_view(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    form = ProjectStageInitialPetitionForm(request.POST, instance=project)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("project_stage_characteristics", pk=pk)
-    else:
-        form = ProjectStageInitialPetitionForm(instance=project)
-    return render(
-        request,
-        "project_stage_initial_petition.html",
-        {"project": project, "form": form},
-    )
-
-
-def project_stage_characteristics_view(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    form = ProjectCharacteristicsForm(request.POST, instance=project)
-    if request.method == "POST":
-        if form.is_valid():
-            newproject = form.save()
-            newproject.notify_new_request_to_ateneu()
-            newproject.notify_request_confirmation()
-            return redirect("project_info")
-    else:
-        form = ProjectCharacteristicsForm(instance=project)
-    return render(
-        request,
-        "project_stage_characteristics.html",
-        {"project": project, "form": form},
-    )
