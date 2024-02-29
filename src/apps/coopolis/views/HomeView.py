9256258c2e01c1fb97eca6873a92c59b124d172f
eliashz <eliashz@gmail.com>
2024-02-29 10:23:53 +0100

    Corregido error an AnonymousUser desde el home

--- src/apps/coopolis/views/HomeView.py
+++ src/apps/coopolis/views/HomeView.py
@@ -1,11 +1,19 @@
-from apps.cc_courses.models import Activity, ActivityEnrolled
-from apps.coopolis.views import LoginSignupContainerView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+from apps.cc_courses.models import Activity, ActivityEnrolled
 from apps.coopolis.models import Project
+from apps.coopolis.views import LoginSignupContainerView
+
 
 class HomeView(LoginSignupContainerView):
     template_name = "home.html"
     model = Activity
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
