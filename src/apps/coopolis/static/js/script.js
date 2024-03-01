d300c820a20c184f026b1e24a210661028d2c9c9
MartaRF3 <ricartfm@gmail.com>
2024-03-01 11:20:00 +0100

    #180 Sortir llista d'espera fixed

--- src/apps/coopolis/static/js/script.js
+++ src/apps/coopolis/static/js/script.js
@@ -133,7 +133,18 @@ function closeThisDropdown(event) {
 
 function showDeleteModal(event) {
   window.scrollTo(0,0)
-  const card = event.target.parentElement.parentElement.parentElement
+  const card = event.parentElement.parentElement.parentElement
+  console.log(event);
+  console.log(card);
+  const modal = card.querySelector(".delete-modal")
+  modal.classList.remove("is-hidden")
+}
+
+function deleteModalWaitingList(event) {
+  window.scrollTo(0,0)
+  const card = event.parentElement.parentElement.parentElement.parentElement.parentElement
+  console.log(event);
+  console.log(card);
   const modal = card.querySelector(".delete-modal")
   modal.classList.remove("is-hidden")
 }
