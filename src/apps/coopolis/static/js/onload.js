a9a19773ea932bd85827fbb95d5a385732be4666
MartaRF3 <ricartfm@gmail.com>
2024-06-26 11:54:34 +0200

    Fix bugs conflictes maquetacions antics forms

--- src/apps/coopolis/static/js/onload.js
+++ src/apps/coopolis/static/js/onload.js
@@ -3,8 +3,12 @@
 function onLoadFunction(){
   // Trobar els inputs de la pàgina
   const allInputs = document.querySelectorAll("input")
-  // Afegint clases pels diferents tipus de inputs
   allInputs.forEach(el => {
+    // Afegint clases pels diferents tipus de inputs
+    if (el.type === "text" || el.type === "email" || el.type === "password") {
+      let parent = el.parentElement
+      parent.classList.add("field")
+    }
     if (el.type === "checkbox") {
       let checkboxParent = el.parentElement
       checkboxParent.classList.add("field-checkbox")
@@ -14,17 +18,20 @@ function onLoadFunction(){
       let labelGeneralRadio = radioParent.previousElementSibling
       let superParent = radioParent.parentElement
       superParent.classList.remove("field")
+      radioParent.classList.add("field")
       radioParent.classList.add("field-radio")
       labelGeneralRadio.classList.add("label-radio")
     }
     if (el.type === "number") {
       let numberParent = el.parentElement
       numberParent.classList.add("field-number")
+      numberParent.classList.add("field")
     }
     if (el.type === "file") {
       let fileParent = el.parentElement
       let label = el.previousElementSibling
       fileParent.classList.add("field-file")
+      fileParent.classList.add("field")
       label.classList.add("upload-icon")
 
       if (fileParent.classList.contains("field-checkbox")) {
@@ -38,12 +45,14 @@ function onLoadFunction(){
   allSelects.forEach(el => {
     let selectParent = el.parentElement
     selectParent.classList.add("field-select")
+    selectParent.classList.add("field")
   })
 
   // Afegint clases per les textareas
   const allTeextareas = document.querySelectorAll("textarea")
   allTeextareas.forEach(el => {
     let textareaParent = el.parentElement
+    textareaParent.classList.add("field")
     textareaParent.classList.add("field-textarea")
   })
 
@@ -68,17 +77,19 @@ function onLoadFunction(){
   if (projectPage || registerPage || profilePage || supportPage || signupPage) {
     // Mostrant districte només si Barcelona
     const townInput = document.querySelector("#id_town")
-    const districtField = document.querySelector("#id_district").parentElement
-    districtField.classList.add("is-hidden")
-    townInput.onchange = toggleDistrict
-    
-    if (townInput.value === "90") {
-      districtField.classList.remove("is-hidden")
-    } else {
+    const districtInput = document.querySelector("#id_district")
+    if (districtInput) {
+      const districtField = districtInput.parentElement
       districtField.classList.add("is-hidden")
-      districtField.value = ""
+      townInput.onchange = toggleDistrict
+      
+      if (townInput.value === "90") {
+        districtField.classList.remove("is-hidden")
+      } else {
+        districtField.classList.add("is-hidden")
+        districtField.value = ""
+      }
     }
-    
   }
 
   // // Pàgina de perfil
