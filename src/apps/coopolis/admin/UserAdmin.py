bb2585bb2b949d0d8575a7049f71043d2cf17a6d
Pere Picornell <pere@perepicornell.com>
2025-01-14 16:58:50 +0100

    EMAIL_SIGNUP_WELCOME passat a post office. El commit inclou el template per EMAIL_ENROLLMENT_REMINDER

--- src/apps/coopolis/admin/UserAdmin.py
+++ src/apps/coopolis/admin/UserAdmin.py
@@ -12,6 +12,7 @@ from django.utils.safestring import mark_safe
 from apps.cc_courses.models import ActivityEnrolled
 from apps.coopolis.forms import MySignUpAdminForm
 from apps.dataexports.models import SubsidyPeriod
+from conf.post_office import send
 
 
 class ActivityEnrolledInline(admin.TabularInline):
@@ -338,14 +339,14 @@ class UserAdmin(admin.ModelAdmin):
             self.send_welcome_email(obj)
 
     def send_welcome_email(self, user_obj):
-        # to post-office
-        # mail = MyMailTemplate("EMAIL_SIGNUP_WELCOME")
-        # mail.subject_strings = {"ateneu_nom": config.PROJECT_FULL_NAME}
-        # mail.body_strings = {
-        #     "ateneu_nom": config.PROJECT_FULL_NAME,
-        #     "url_backoffice": settings.ABSOLUTE_URL,
-        #     "url_accions": f"{settings.ABSOLUTE_URL}{reverse('courses')}",
-        #     "url_projecte": f"{settings.ABSOLUTE_URL}{reverse('project_info')}",
-        # }
-        # mail.send_to_user(user_obj)
-        pass
+        context = {
+            "ateneu_nom": config.PROJECT_FULL_NAME,
+            "url_backoffice": settings.ABSOLUTE_URL,
+            "url_accions": f"{settings.ABSOLUTE_URL}{reverse('courses')}",
+            "url_projecte": f"{settings.ABSOLUTE_URL}{reverse('project_info')}",
+        }
+        send(
+            recipients=user_obj.email,
+            template="EMAIL_SIGNUP_WELCOME",
+            context=context,
+        )
--- src/apps/coopolis/migrations/0133_data_postoffce_emails.py
+++ src/apps/coopolis/migrations/0133_data_postoffce_emails.py
@@ -23,6 +23,9 @@ def populate_mail_templates(apps, schema_editor):
   </td>
 </tr>
 <tr>
+  <td height="15" style="font-size:0px; line-height:0px; height:15px;">&nbsp;</td>
+</tr>
+<tr>
   <td align="left" valign="top" style="padding: 0px 40px 10px 40px" bgcolor="#fafafa" class="em_text1 pad10">
     <p style="margin-left: 60px"><strong>Nom del projecte:</strong> {{projecte_nom}}</p>
     <p style="margin-left: 60px"><strong>Telèfon de contacte:</strong> {{projecte_telefon}}</p>
@@ -50,6 +53,9 @@ def populate_mail_templates(apps, schema_editor):
   </td>
 </tr>
 <tr>
+  <td height="15" style="font-size:0px; line-height:0px; height:15px;">&nbsp;</td>
+</tr>
+<tr>
   <td align="left" valign="top" style="padding: 0px 40px 10px 40px" bgcolor="#fafafa" class="em_text1 pad10">
   <p style="padding-top: 20px">En els propers dies una persona de l'equip de 
     l'ateneu cooperatiu es posarà en contacte amb tu per parlar dels propers 
@@ -60,6 +66,90 @@ def populate_mail_templates(apps, schema_editor):
 </tr>
 </table>
 """
+                    ),
+                },
+            },
+        ),
+        dict(
+            id="EMAIL_ENROLLMENT_REMINDER",
+            translated_templates={
+                "ca": {
+                    "subject": "Recordatori d'inscripció a l'activitat: {{activitat_nom}}",
+                    "body": get_default_email_template(
+                        """
+<table align="center" width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f0f0f0">
+<tr>
+  <td align="center" valign="top" class="em_text1 pad10">
+    <p>Recordatori d'inscripció</p>
+  </td>
+</tr>
+<tr>
+  <td height="15" style="font-size:0px; line-height:0px; height:15px;">&nbsp;</td>
+</tr>
+<tr>
+  <td align="center" valign="top" style="font-family:'Open Sans', Arial, sans-serif; font-size:22px;
+  line-height:22px; color:#000; letter-spacing:2px; padding-bottom:12px;" class="pad10">
+    <strong>{{activitat_nom}}</strong> de {{ateneu_nom}}
+  </td>
+</tr>
+<tr>
+  <td height="25" class="em_h20" style="font-size:0px; line-height:0px; height:25px;">&nbsp;</td>
+</tr>
+<tr>
+  <td align="left" valign="top" style="padding: 0px 40px 10px 40px" bgcolor="#fafafa" class="em_text1 pad10">
+    <p style="margin-left: 60px"><strong>Data:</strong> {{activitat_data_inici}}</p>
+    <p style="margin-left: 60px"><strong>Hora:</strong> {{activitat_hora_inici}}</p>
+    <p style="margin-left: 60px"><strong>Lloc:</strong> {{activitat_lloc}}</p>
+    <p style="padding-top: 20px">Es tracta d'una formació gratuïta subvencionada, per això et demanem que, si
+      finalment no pots venir, ens avisis amb antelació per poder obrir la plaça a una altra persona.<br>
+      Per fer-ho pots gestionar les teves inscripcions accedint a l'aplicació amb el teu compte i anant a 
+    l'apartat <a href="{{absolute_url_my_activities}}">Perfil -> Els Meus Cursos</a>.</p>
+  </td>
+</tr>
+<tr>
+  <td align="center" class="em_text1 pad10" bgcolor="#fafafa" style="padding-bottom: 15px;">
+    <a href="{{url_web_ateneu}}" style="color:#e94e1b; text-decoration:none; font-weight: bold;">Fins aviat!</a>
+  </td>
+</tr>
+</table>
+"""
+                    ),
+                },
+            },
+        ),
+        dict(
+            id="EMAIL_SIGNUP_WELCOME",
+            translated_templates={
+                "ca": {
+                    "subject": "Nou compte creat a {{ateneu_nom}}",
+                    "body": get_default_email_template(
+                        """
+<table align="center" width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f0f0f0">
+<tr>
+  <td align="center" valign="top" class="em_text1 pad10">
+    <p>Benvingut/da a {{ateneu_nom}}!</p>
+  </td>
+</tr>
+<tr>
+  <td height="15" style="font-size:0px; line-height:0px; height:15px;">&nbsp;</td>
+</tr>
+<tr>
+  <td align="left" valign="top" style="padding: 0px 40px 10px 40px" bgcolor="#fafafa" class="em_text1 pad10">
+  <p style="padding-top: 20px"><em>Estàs rebent aquest correu perquè s'ha completat un registre a la plataforma <a href=\"{{url_backoffice}}\">{{url_backoffice}}</a>.<br />
+    Si aquest registre no l'has fet tu o cap altra persona amb qui comparteixis aquest compte, ignora aquest correu o 
+    avisa'ns per tal que l'eliminem de la base de dades.</em></p>
+  <p style="padding-top: 20px">Amb el teu compte pots:</p>
+  <ul>
+    <li>Inscriure't a les sessions formatives, que trobaràs <a href="{{url_accions}}">aquí</a>.</li>
+    <li>Si esteu iniciant o teniu en marxa un projecte cooperatiu, podeu <a href="{{url_projecte}}">sol·licitar un acompanyament</a>.</li>
+    <li>Consultar o editar les dades del teu perfil i recuperar la contrassenya. </li>
+  </ul>
+
+  <p style="padding-top: 20px">Més informació a <a href="{{url_backoffice}}">{{url_backoffice}}</a>.</p>
+  </td>
+</tr>
+</table>
+""",
                     ),
                 },
             },
